a894aaafc2caaf07c9f3ba4bf3b4582ac61f6347
Manny Patrick Vivas <mannyvivas21@gmail.com>
2026-05-22 05:00:05 -0400

    Committal update

--- Assets/Scripts/Levels/Lantern/MoveLantern.cs
+++ Assets/Scripts/Levels/Lantern/MoveLantern.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class MoveLantern : MonoBehaviour
 {
-    [SerializeField] GameObject burnable;
     [SerializeField] Transform target;
+    OscillateObject floatingObj;
     bool inPosition;
+    public bool move, turnOnDebrisOscillation;
+
+    void Start()
+    {
+        floatingObj = GetComponent<OscillateObject>();
+    }
 
     void Update()
     {
-        if (burnable == null && !inPosition) StartCoroutine(MoveToPosition(target.position));
+        //if (burnable == null && !inPosition) StartCoroutine(MoveToPosition(target.position));
+
+        if (move)
+        {
+            move = false;
+            StartCoroutine(MoveToTarget(target.position));
+        }
+    }
+
+
+    IEnumerator MoveToTarget(Vector3 target)
+    {
+        floatingObj.pause = true;
+
+        Vector3 start = transform.position;
+        Vector3 end = target;
+
+        float elapsed = 0f;
+        float duration = 3f;
+
+        // lerp to target
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(start, end, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = end;
+        floatingObj.pos = floatingObj.transform.position;
+        floatingObj.pause = false;
+        turnOnDebrisOscillation = true;
     }
     //Thanks, Josh!
     IEnumerator MoveToPosition(Vector3 target)
